31509776de55f1e8d3ff6cb29e5c4dc41dc6eb2b
Rodolfo HiOk <rodolfo.okino@gmail.com>
2025-07-24 17:24:44 -0300

    feat: config microservices with eurika client

--- algadelivery/microsservices/Delivery-Tracking/src/main/java/br/com/hiokdev/algadelivery/delivery/tracking/infrastructure/http/client/CourierAPIClientConfig.java
+++ algadelivery/microsservices/Delivery-Tracking/src/main/java/br/com/hiokdev/algadelivery/delivery/tracking/infrastructure/http/client/CourierAPIClientConfig.java
@@ -1,6 +1,7 @@
 package br.com.hiokdev.algadelivery.delivery.tracking.infrastructure.http.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -10,12 +11,18 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 public class CourierAPIClientConfig {
 
-    @Value("${app.courier-api-url}")
-    private String courierApiUrl;
+    @Value("${app.courier-api-name}")
+    private String courierApiName;
+
+    @Bean
+    @LoadBalanced
+    public RestClient.Builder loadBalacendRestClientBuilder() {
+        return RestClient.builder();
+    }
 
     @Bean
     public CourierAPIClient courierAPIClient(RestClient.Builder builder) {
-        RestClient restClient = builder.baseUrl(courierApiUrl).build();
+        RestClient restClient = builder.baseUrl(courierApiName).build();
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builderFor(adapter).build();
         return proxyFactory.createClient(CourierAPIClient.class);
