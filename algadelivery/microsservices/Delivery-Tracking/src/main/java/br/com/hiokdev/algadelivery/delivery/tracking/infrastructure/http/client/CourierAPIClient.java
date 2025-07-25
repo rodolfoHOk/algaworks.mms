0d57a5d667eeb639de5f934e1d8a7ca7e5b1da25
Rodolfo HiOk <rodolfo.okino@gmail.com>
2025-07-24 22:05:51 -0300

    feat: implements retry in http client requests

--- algadelivery/microsservices/Delivery-Tracking/src/main/java/br/com/hiokdev/algadelivery/delivery/tracking/infrastructure/http/client/CourierAPIClient.java
+++ algadelivery/microsservices/Delivery-Tracking/src/main/java/br/com/hiokdev/algadelivery/delivery/tracking/infrastructure/http/client/CourierAPIClient.java
@@ -2,6 +2,7 @@ package br.com.hiokdev.algadelivery.delivery.tracking.infrastructure.http.client
 
 import br.com.hiokdev.algadelivery.delivery.tracking.infrastructure.http.dto.CourierPayoutCalculationInput;
 import br.com.hiokdev.algadelivery.delivery.tracking.infrastructure.http.dto.CourierPayoutResultModel;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
@@ -10,6 +11,7 @@ import org.springframework.web.service.annotation.PostExchange;
 public interface CourierAPIClient {
 
     @PostExchange("/payout-calculation")
+    @Retry(name = "Retry_CourierAPIClient_payoutCalculation")
     CourierPayoutResultModel payoutCalculation(@RequestBody CourierPayoutCalculationInput input);
 
 }
